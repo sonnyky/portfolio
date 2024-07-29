1548bcc3751f741b59f7a557b69045cf6a78a17d
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2024-07-29 14:41:33 +0900

    Initial commit
    initial commit

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const testimonials = [
+{
+    name: "Chening Yang",
+    company: "Cinnamon AI",
+    quote:
+      "Having had the pleasure of working closely with Sonny, I can attest to his remarkable skills and invaluable contributions. Sonny is a highly capable developer and also an exceptional communicator. This combination of technical prowess and effective communication is a rarity in our field. His ability to seamlessly blend both aspects sets him apart from his peers. Sonny possesses a versatility that is truly impressive, he excels in areas where others may struggle to find balance. One of Sonny's most notable qualities is his logical thinking and eloquence. He has a remarkable ability to articulate complex ideas with clarity and precision. His logical approach to problem-solving has consistently led to innovative solutions and successful outcomes. In my years of experience, Sonny stands out as one of the most competent and reliable team members I have ever had the pleasure of working with. His dedication, professionalism, and exceptional skills make him an invaluable asset to any organization. I have no doubt that Sonny will excel in any role he undertakes.",
+    image: "./jeff.jpg",
+}
+];
+
+export const skills = [
+  "Computer Vision",
+  "Unity (Games, Simulation, Digital Twin)",
+  "Machine Learning",
+  "Programming (C++, Python, C#)",
+  "Robotics",
+  "Project Management",
+  "Product Management"
+];
